98d90fa7608883860044f4f769a41d9ac21e9a74
Bilal Ozdemir <Bozdemir2975@conestogac.on.ca>
2023-10-05 09:11:39 -0400

    Add project files.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace TicTacToe
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cell9 = new System.Windows.Forms.PictureBox();
+			this.cell8 = new System.Windows.Forms.PictureBox();
+			this.cell7 = new System.Windows.Forms.PictureBox();
+			this.cell6 = new System.Windows.Forms.PictureBox();
+			this.cell3 = new System.Windows.Forms.PictureBox();
+			this.cell5 = new System.Windows.Forms.PictureBox();
+			this.cell2 = new System.Windows.Forms.PictureBox();
+			this.cell4 = new System.Windows.Forms.PictureBox();
+			this.cell1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.cell9)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell8)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell7)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell6)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// cell9
+			// 
+			this.cell9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.cell9.Location = new System.Drawing.Point(435, 256);
+			this.cell9.Name = "cell9";
+			this.cell9.Size = new System.Drawing.Size(77, 71);
+			this.cell9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.cell9.TabIndex = 1;
+			this.cell9.TabStop = false;
+			// 
+			// cell8
+			// 
+			this.cell8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.cell8.Location = new System.Drawing.Point(362, 256);
+			this.cell8.Name = "cell8";
+			this.cell8.Size = new System.Drawing.Size(77, 71);
+			this.cell8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.cell8.TabIndex = 2;
+			this.cell8.TabStop = false;
+			// 
+			// cell7
+			// 
+			this.cell7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.cell7.Location = new System.Drawing.Point(288, 256);
+			this.cell7.Name = "cell7";
+			this.cell7.Size = new System.Drawing.Size(77, 71);
+			this.cell7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.cell7.TabIndex = 3;
+			this.cell7.TabStop = false;
+			// 
+			// cell6
+			// 
+			this.cell6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.cell6.Location = new System.Drawing.Point(435, 191);
+			this.cell6.Name = "cell6";
+			this.cell6.Size = new System.Drawing.Size(77, 71);
+			this.cell6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.cell6.TabIndex = 4;
+			this.cell6.TabStop = false;
+			// 
+			// cell3
+			// 
+			this.cell3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.cell3.Location = new System.Drawing.Point(435, 124);
+			this.cell3.Name = "cell3";
+			this.cell3.Size = new System.Drawing.Size(77, 71);
+			this.cell3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.cell3.TabIndex = 5;
+			this.cell3.TabStop = false;
+			// 
+			// cell5
+			// 
+			this.cell5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.cell5.Location = new System.Drawing.Point(362, 191);
+			this.cell5.Name = "cell5";
+			this.cell5.Size = new System.Drawing.Size(77, 71);
+			this.cell5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.cell5.TabIndex = 6;
+			this.cell5.TabStop = false;
+			// 
+			// cell2
+			// 
+			this.cell2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.cell2.Location = new System.Drawing.Point(362, 124);
+			this.cell2.Name = "cell2";
+			this.cell2.Size = new System.Drawing.Size(77, 71);
+			this.cell2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.cell2.TabIndex = 7;
+			this.cell2.TabStop = false;
+			// 
+			// cell4
+			// 
+			this.cell4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.cell4.Location = new System.Drawing.Point(288, 191);
+			this.cell4.Name = "cell4";
+			this.cell4.Size = new System.Drawing.Size(77, 71);
+			this.cell4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.cell4.TabIndex = 8;
+			this.cell4.TabStop = false;
+			// 
+			// cell1
+			// 
+			this.cell1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.cell1.Location = new System.Drawing.Point(288, 124);
+			this.cell1.Name = "cell1";
+			this.cell1.Size = new System.Drawing.Size(77, 71);
+			this.cell1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.cell1.TabIndex = 9;
+			this.cell1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.cell9);
+			this.Controls.Add(this.cell8);
+			this.Controls.Add(this.cell7);
+			this.Controls.Add(this.cell6);
+			this.Controls.Add(this.cell3);
+			this.Controls.Add(this.cell5);
+			this.Controls.Add(this.cell2);
+			this.Controls.Add(this.cell4);
+			this.Controls.Add(this.cell1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.cell9)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell8)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell7)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell6)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.cell1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox cell9;
+		private System.Windows.Forms.PictureBox cell8;
+		private System.Windows.Forms.PictureBox cell7;
+		private System.Windows.Forms.PictureBox cell6;
+		private System.Windows.Forms.PictureBox cell3;
+		private System.Windows.Forms.PictureBox cell5;
+		private System.Windows.Forms.PictureBox cell2;
+		private System.Windows.Forms.PictureBox cell4;
+		private System.Windows.Forms.PictureBox cell1;
+	}
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+	public partial class Form1 : Form
+	{
+		// Accessing the images. 
+		Image imageX = TicTacToe.Properties.Resources.x;
+		Image imageO = TicTacToe.Properties.Resources.o;
+
+		// Creating an array for the pictureboxes
+		public PictureBox[] cells;
+		bool xTurn = true;
+		public Form1()
+		{
+
+			InitializeComponent();
+			// Initializing my cells
+			cells = new PictureBox[] { cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9 };
+
+			foreach (PictureBox cell in cells)
+			{
+				// Activating my CellClick method everytime a cell is clicked.
+				cell.Click += CellClick;
+			}
+
+		}
+		/// <summary>
+		/// This is method acts based on the user's cell clicks.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void CellClick(object sender, EventArgs e)
+		{
+			PictureBox cellClicked = (PictureBox)sender;
+
+			if (cellClicked.Image != null)
+			{
+
+				return;
+			}
+
+			if (xTurn)
+			{
+				cellClicked.Image = imageX;
+			}
+			else
+			{
+				cellClicked.Image = imageO;
+			}
+			xTurn = !xTurn;
+
+
+
+			if (CheckWinner())
+			{
+				// Displaying the win messages based on who won.
+				if (xTurn)
+				{
+					MessageBox.Show("O Wins!!!");
+					GameReset();
+					return;
+				}
+				else
+				{
+					MessageBox.Show("X Wins!!!");
+					GameReset();
+					return;
+				}
+
+			}
+			// Displaying the draw message if there is no winner after all the boxes are filled.
+			if (AllCellsFilled())
+			{
+				MessageBox.Show("It's a Draw!!!");
+				GameReset();
+				return;
+			}
+
+
+
+		}
+
+		/// <summary>
+		/// This method is checking the win condition.
+		/// It checks the rows, columns, and diagonals.
+		/// </summary>
+		/// <returns></returns>
+		public bool CheckWinner()
+		{
+			// Loop for checking rows.
+			for (int r = 0; r < 9; r += 3)
+			{
+				// Checking the winning conditions for rows. 
+				if (cells[r].Image != null && cells[r].Image == cells[r + 1].Image && cells[r + 1].Image == cells[r + 2].Image)
+				{
+					return true;
+				}
+			}
+
+			// Loop for checking columns
+			for (int c = 0; c < 3; c++)
+			{
+				// Checking the winning conditions for columns. 
+				if (cells[c].Image != null && cells[c].Image == cells[c + 3].Image && cells[c + 3].Image == cells[c + 6].Image)
+				{
+					return true;
+				}
+			}
+
+
+			// Checking Diagonal (from top left to bottom right)
+			if (cells[0].Image != null && cells[0].Image == cells[4].Image && cells[4].Image == cells[8].Image)
+			{
+				return true;
+			}
+
+			// Checking Diagonal (from top right to bottom left)
+			else if (cells[2].Image != null && cells[2].Image == cells[4].Image && cells[4].Image == cells[6].Image)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// This method checks if all the cells are filled. 
+		/// </summary>
+		/// <returns></returns>
+		public bool AllCellsFilled()
+		{
+			// This loop checks if the cells are not null. If they are not all filled, it returns false.
+			// If they are all filled, it returns true. 
+			foreach (PictureBox cell in cells)
+			{
+				if (cell.Image == null)
+				{
+					return false;
+				}
+
+			}
+			return true;
+
+		}
+
+		/// <summary>
+		/// This method resets the game after the result is displayed. 
+		/// </summary>
+		public void GameReset()
+		{
+			// Reseting all the cells.
+			foreach (PictureBox cell in cells)
+			{
+				cell.Image = null;
+			}
+			// Giving the turn to the x (since it's the start of the game).
+			xTurn = true;
+		}
+
+
+
+	}
+}
